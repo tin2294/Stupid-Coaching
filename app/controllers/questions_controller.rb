749aa2813c63e416c811aa9f2610d2311cf89704
Ting Ting Chen <tin2294@gmail.com>
2021-11-08 12:55:52 +0100

    stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+    params[:question]
+  end
+
+  def answer
+    if params[:question][-1] == '?'
+      return @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:question] == 'I am going to work'
+      return @answer = 'Great!'
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
